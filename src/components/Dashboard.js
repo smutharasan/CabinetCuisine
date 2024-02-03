7e9f52f052ee4895822b32c09573f4d62d2c6b59
Zara Farrukh <zara.farrukh@ontariotechu.net>
2024-02-03 17:38:45 -0500

    Dashboard update

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import '../index.css';
 
-// Separate RestaurantCard component
-const RestaurantCard = ({ name, cuisine, rating, deliveryTime, imageUrl }) => (
-  <div className="restaurant-card">
+// Separate RecipeCard component
+const RecipeCard = ({ name, cuisine, rating, cookingTime, imageUrl }) => (
+  <div className="recipe-card">
     <img src={imageUrl} alt={name} />
-    <div className="restaurant-details">
+    <div className="recipe-details">
       <h3>{name}</h3>
       <p>Cuisine: {cuisine}</p>
       <p>Rating: {rating}</p>
-      <p>Delivery Time: {deliveryTime} mins</p>
+      <p>Cooking Time: {cookingTime} mins</p>
     </div>
   </div>
 );
 
 const Dashboard = () => {
-  const restaurants = [
+  const recipe = [
     {
-      name: 'Sample Restaurant 1',
-      cuisine: 'Italian',
+      name: 'Butter Chicken',
+      cuisine: 'Indian',
       rating: 4.5,
-      deliveryTime: 30,
-      imageUrl: 'https://placekitten.com/200/300', // Replace with actual image URL
+      cookingTime: 30,
+      imageUrl: 'https://images.unsplash.com/photo-1588166524941-3bf61a9c41db?q=80&w=1884&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     },
-    // Add more restaurant objects
+
+    {
+      name: 'Japanese Sushi and Sashimi',
+      cuisine: 'Japanese',
+      rating: 4.8,
+      cookingTime: 50,
+      imageUrl: 'https://images.unsplash.com/photo-1601059286024-61032e83b203?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
   ];
 
   return (
@@ -32,9 +39,9 @@ const Dashboard = () => {
         <h1>Welcome to PantryPal</h1>
         {/* Add navigation or other header components */}
       </header>
-      <section className="restaurant-list">
-        {restaurants.map((restaurant, index) => (
-          <RestaurantCard key={index} {...restaurant} />
+      <section className="recipe-list">
+        {recipe.map((recipe, index) => (
+          <RecipeCard key={index} {...recipe} />
         ))}
       </section>
       {/* Add other sections or components as needed */}
