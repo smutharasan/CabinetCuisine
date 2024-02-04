1ab256b30a4ca5db2e33333f19ebb1447ef9523c
Zara Farrukh <zara.farrukh@ontariotechu.net>
2024-02-04 02:04:38 -0500

    Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="contact">
+        <p>Contact us: PantryPal@business.com | Call +1(234) 567 8901</p>
+      </div>
+      <div className="socials">
+        <a href="#" target="_blank" rel="noopener noreferrer">Twitter</a>
+        <a href="#" target="_blank" rel="noopener noreferrer">Instagram</a>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
