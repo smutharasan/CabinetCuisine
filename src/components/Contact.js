d42594500404811a409cf06247f6a1d468f36c28
Zara Farrukh <zara.farrukh@ontariotechu.net>
2024-02-04 03:39:32 -0500

    Contact updated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,21 @@
 // Contact.js
 import React from "react";
+import Footer from "./Footer";
+import { Link } from 'react-router-dom';
 
 const Contact = () => {
   return (
     <div>
       <h1>Contact Page</h1>
-      {/* Add your contact page content here */}
+      Names: Zara Farrukh
+            Supriya Mutharasan
+            Rosie Khurmi
+            Rabia Chattha
+            Nimra Rehman
+      Email: PantryPal@business.com
+      Call: +1(234) 567 8910
+
+      <Footer />
     </div>
   );
 };
